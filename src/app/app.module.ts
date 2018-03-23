52557f9b2103e004c43d6973b1bb52a7652821bd
Amruta T <amruta.thakur@exultcorp.com>
2018-03-23 19:32:51 +0530

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+
+const ROUTES : Routes = [
+  { path: "", component: HomeComponent },
+  { path: "blog", component: BlogComponent },
+  { path: "about", component: AboutComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
